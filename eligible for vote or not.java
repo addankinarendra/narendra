fc073d5451af15084525cd153d687f0bf70f1ff4
addankinarendra <addankinarendrakumar33@gmail.com>
2024-08-01 11:29:45 +0530

    2nd commit

---
+++ eligible for vote or not.java
@@ -0,0 +1,23 @@
+class Helloworld{
+	public static void main(String args[]){
+	int age=18,gr=2020;
+	String gd="graduate";
+	if(age>=25)
+	{
+		if(gr<=2020)
+		{
+		System.out.println("eleigible for MLC vote);
+	}
+	else if(age>=50)
+	{
+		if(gd=="graduate")
+	{
+		System.out.println("eleigible for MLA vote);
+	}
+		else{
+		System.out.println("not eligible for vote");
+	}
+	
+
+	
+	
